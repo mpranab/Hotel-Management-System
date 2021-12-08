9f08eed10968f97acdbe616b72a7a913e25d7772
mpranab <pranab-kumar.mohanta@capgemini.com>
2021-12-08 11:06:00 +0530

    Day 2

---
+++ Department-Microservice/src/main/java/com/Department/Controller/DepartmentController.java
@@ -0,0 +1,59 @@
+package com.Department.Controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Department.Model.Department;
+import com.Department.Model.DepartmentList;
+import com.Department.Service.Departmentservice;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+
+	@Autowired
+	private Departmentservice service;
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Department!!";
+	}
+
+	@PostMapping("/add")
+	public Department addDept(@RequestBody Department dept) {
+		return this.service.addDepartment(dept);
+	}
+
+	@GetMapping("/findAll")
+	public DepartmentList getDepartment() {
+
+		DepartmentList list = new DepartmentList();
+		list.setAlldept(this.service.getAlldept());
+		return list;
+	}
+
+	@GetMapping("/findById/{id}")
+	public Optional<Department> getDepartment(@PathVariable("id") String id) {
+
+		return this.service.getDepartment(Long.parseLong(id));
+	}
+
+	@PutMapping("/update")
+	public Department updateDepartment(@RequestBody Department dept) {
+		return this.service.updateDepartment(dept);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String deleteDepartment(@PathVariable("id") String id) {
+		return this.service.deleteDepartment(Long.parseLong(id));
+	}
+}
---
+++ Department-Microservice/src/main/java/com/Department/Repository/DepartmentRepo.java
@@ -0,0 +1,11 @@
+package com.Department.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Department.Model.Department;
+
+@Repository
+public interface DepartmentRepo extends MongoRepository<Department, Long> {
+
+}
